4935dcd9d4b9a4636e4eb680d4260ff21f4354bb
raku10ehime <95738593+raku10ehime@users.noreply.github.com>
2023-05-29 09:54:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -177,7 +177,7 @@ folium.raster_layers.TileLayer(
 
 folium.raster_layers.TileLayer(
     name="楽天モバイル",
-    tiles="https://area-map.mobile.rakuten.co.jp/dsd/geoserver/4g4m/mno_coverage_map/gwc/service/gmaps?LAYERS=mno_coverage_map:all_map&FORMAT=image/png&TRANSPARENT=TRUE&x={x}&y={y}&zoom={z}&update=20220404",
+    tiles="https://area-map.mobile.rakuten.co.jp/dsd/geoserver/4g4m/mno_coverage_map/gwc/service/gmaps?LAYERS=mno_coverage_map:all_map&FORMAT=image/png&TRANSPARENT=TRUE&x={x}&y={y}&zoom={z}&update=20220516",
     fmt="image/png",
     attr="楽天モバイルエリア",
     tms=False,

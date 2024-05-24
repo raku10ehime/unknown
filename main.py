3c25768c23a7cf54b52cf7c96fcd95efbd9f2516
raku10ehime <95738593+raku10ehime@users.noreply.github.com>
2024-05-25 01:41:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,311 +1,138 @@
-import pathlib
-
 import folium
-import folium.plugins
-import geopandas as gpd
 import pandas as pd
-
-import pyproj
-
-pd.set_option("display.max_columns", None)
-
-# MLS
-
-df_mls = pd.read_json("https://mls.js2hgw.com/cellmap/mls44011.json").query(
-    "188743680 <= cell < 190023680"
-)
-
-# 日時に変換
-
-dt_now = pd.Timestamp.now(tz="Asia/Tokyo").tz_localize(None)
-
-df_mls["created"] = pd.to_datetime(df_mls["created"], unit="s")
-df_mls["updated"] = pd.to_datetime(df_mls["updated"], unit="s")
-
-df_mls[["eNB", "LCID"]] = df_mls["cell"].apply(lambda x: pd.Series([x >> 8, x & 0xFF]))
-
-df_mls["id"] = df_mls["eNB"].astype(str) + "-" + df_mls["LCID"].astype(str)
-
-df_mls["経過日数"] = (dt_now - df_mls["updated"]).dt.days
-
-df_ehime = (
-    df_mls.sort_values(by=["updated", "cell"])
-    .drop_duplicates(subset=["cell"], keep="last")
-    .reindex(
-        columns=[
-            "area",
-            "lat",
-            "lon",
-            "created",
-            "updated",
-            "cell",
-            "eNB",
-            "LCID",
-            "id",
-            "市区町村名",
-            "経過日数",
-        ]
-    )
-    .sort_values(by="cell")
-    .reset_index(drop=True)
-)
-
-df_ehime
-
-def enblcid_split(df_tmp):
-    df0 = df_tmp.copy()
-
-    df0["eNB-LCID"] = df0["eNB-LCID"].str.split()
-    df1 = df0.explode("eNB-LCID")
-
-    df1[["eNB", "LCID"]] = df1["eNB-LCID"].str.split("-", expand=True)
-
-    df1["LCID"] = df1["LCID"].str.split(",")
-    df2 = df1.explode("LCID").astype({"eNB": int, "LCID": int})
-
-    df2["cell"] = df2.apply(lambda x: (x["eNB"] << 8) | x["LCID"], axis=1)
-
-    df2 = df2.sort_values(["cell"]).reset_index(drop=True)
-
-    return df2
-
-csv_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTuN5xiHhlnPTkv3auHkYLT9NPvvjayj5AdPrH5VBQdbELOzfONi236Vub6eSshv8jAxQw3V1rgbbgE/pub?gid=882951423&single=true&output=csv"
-
-df_csv = pd.read_csv(
-    csv_url, parse_dates=["更新日時"], usecols=["ID", "更新日時", "場所", "eNB-LCID", "地図"]
-).dropna(how="all")
-
-df_csv
-
-df_csv[["lat", "lon"]] = df_csv["地図"].str.split(",", expand=True).astype(float)
-
-df_enb = (
-    enblcid_split(df_csv.dropna(subset=["eNB-LCID"]))
-    .drop_duplicates(subset=["ID", "cell"], keep="last")
-    .sort_values(by=["cell", "更新日時"])
-    .reset_index(drop=True)
-)
-
-grs80 = pyproj.Geod(ellps="GRS80")
-
-idx = []
-
-for i, r in df_ehime.iterrows():
-    df_tmp = df_enb[df_enb["cell"] == r.cell].copy()
-
-    for j, t in df_tmp.iterrows():
-        n = grs80.inv(r.lon, r.lat, t.lon, t.lat)[2]
-        
-        df_ehime.at[i, "name"] = t["場所"]
-        df_ehime.at[i, "distance"] = n
-
-        if n < 5000:
-            idx.append(i)
-
-dft = df_ehime.drop(set(idx)).copy()
-
-# 180日以内
-unknown = dft[dft["経過日数"] < 180].copy()
-
-df0 = (
-    pd.read_csv(
-        "https://raku10ehime.github.io/map/ehime.csv", index_col=0, parse_dates=["更新日時"]
-    )
-    .dropna(how="all")
-)
-
-df_map = enblcid_split(df0.dropna(subset=["eNB-LCID"])).sort_values(by=["cell", "更新日時"]).reset_index(drop=True)
-
-df_map
-
-df1 = pd.merge(df_map, df_ehime, on="cell", how="left")
-
-df1[["eNB", "LCID"]] = df1["cell"].apply(lambda x: pd.Series([x >> 8, x & 0xFF]))
-df1["id"] = df1["eNB"].astype(str) + "-" + df1["LCID"].astype(str)
+import streamlit as st
+from streamlit_folium import st_folium
 
 
-df1["距離"] = df1.apply(lambda x: grs80.inv(x["経度"], x["緯度"], x.lon, x.lat)[2], axis=1)
+def enblcid_split(df0):
+    df1 = df0.copy()
 
-df1["更新日時"].mask(df1["更新日時"] < df1.updated, df1.updated, inplace=True)
+    df1["eNB-LCID"] = df1["eNB-LCID"].str.split()
+    df2 = df1.explode("eNB-LCID")
 
-df1["経過日数"] = (dt_now - df1["更新日時"]).dt.days
+    df2[["eNB", "LCID"]] = df2["eNB-LCID"].str.split("-", expand=True)
+    df2["LCID"] = df2["LCID"].str.split(",")
+    df3 = df2.explode("LCID").astype({"eNB": int, "LCID": int})
 
-df1.sort_values(by=["cell"], inplace=True)
+    df3["cell_id"] = df3.apply(lambda x: (x["eNB"] << 8) | x["LCID"], axis=1)
 
-df1
+    result = df3.sort_values(["cell_id"]).reset_index(drop=True)
 
-df2 = (
-    df1.groupby(by=["場所", "eNB-LCID", "緯度", "経度", "color", "icon"])
-    .agg({"更新日時": [min, list], "経過日数": max})
-    .droplevel(level=0, axis=1)
-    .rename(columns={"min": "update", "list": "更新日時", "max": "経過日数"})
-    .reset_index()
-)
+    return result
 
-df2["更新日時"] = df2["更新日時"].apply(lambda x: "\n".join(i.strftime("%Y/%m/%d") for i in x))
 
-df2["past_days"] = pd.cut(
-    df2["経過日数"],
-    [0, 90, 180, 360, 720, 99999],
-    labels=["green", "lime", "yellow", "red", "white"],
-    right=False,
-)
+st.set_page_config(page_title="TowerCollectorファイル分析")
+st.title("TowerCollectorファイル分析")
 
-df2
 
-# 地図
+uploaded_files = st.file_uploader("TowerCollector CSV", type="csv", key="csv", accept_multiple_files=True)
 
-colors = {
-    0: "darkblue",
-    1: "lightred",
-    2: "lightgreen",
-    3: "lightblue",
-    4: "darkred",
-    5: "darkgreen",
-}
-
-map = folium.Map(
-    tiles=None,
-    location=[34.06604300, 132.99765800],
-    zoom_start=12,
-)
-
-folium.raster_layers.TileLayer(
-    tiles="https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png",
-    name="国土地理院",
-    attr='&copy; <a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>',
-).add_to(map)
-
-folium.raster_layers.TileLayer(
-    name="楽天モバイル",
-    tiles="https://area-map.mobile.rakuten.co.jp/5gs/geoserver/5g/mno_coverage_map/gwc/service/gmaps?LAYERS=mno_coverage_map:all_map&FORMAT=image/png&TRANSPARENT=TRUE&x={x}&y={y}&zoom={z}&update=20231130",
-    fmt="image/png",
-    attr="楽天モバイルエリア",
-    tms=False,
-    overlay=True,
-    control=True,
-    opacity=1,
-    show=False,
-).add_to(map)
-
-folium.raster_layers.TileLayer(
-    name="楽天モバイル（自社）",
-    tiles="https://area-map.mobile.rakuten.co.jp/turbo/geoserver/4g/mno_coverage_map/gwc/service/gmaps?LAYERS=mno_coverage_map:all_map&FORMAT=image/png&TRANSPARENT=TRUE&&x={x}&y={y}&zoom={z}",
-    fmt="image/png",
-    attr="楽天モバイル（自社）",
-    tms=False,
-    overlay=True,
-    control=True,
-    opacity=1,
-).add_to(map)
-
-folium.raster_layers.TileLayer(
-    name="au Band3",
-    tiles="https://area.uqcom.jp/api2/4G_17/{z}/{x}/{y}.png",
-    fmt="image/png",
-    attr="au Band3エリア",
-    tms=False,
-    overlay=True,
-    control=True,
-    opacity=0.5,
-    show=False,
-).add_to(map)
-
-fg1 = folium.FeatureGroup(name="未発見").add_to(map)
-fg2 = folium.FeatureGroup(name="エリア外").add_to(map)
-fg3 = folium.FeatureGroup(name="基地局").add_to(map)
-fg4 = folium.FeatureGroup(name="サークル", show=False).add_to(map)
-fg5 = folium.FeatureGroup(name="更新状況").add_to(map)
-
-for i, r in unknown.iterrows():
-    
-    if pd.isnull(r["distance"]):
-    
-        fg1.add_child(
-            folium.Circle(
-                location=[r.lat, r.lon],
-                popup=folium.Popup(f'<p>{r["id"]}</p><p>{r["updated"]}</p><p>{r["name"]}</p><p>{r["distance"]}</p>', max_width=300),
-                tooltip=f'<p>{r["id"]}</p><p>{r["updated"]}</p><p>{r["name"]}</p><p>{r["distance"]}</p>',
-                radius=800,
-                color=colors.get(r["LCID"] % 6),
-            )
+if uploaded_files:
+    dfs = [
+        pd.read_csv(uploaded_file, parse_dates=["measured_at", "discovered_at"], dtype={"ta": str}).dropna(
+            how="all", axis=1
         )
-    else:
-        fg2.add_child(
-            folium.Circle(
-                location=[r.lat, r.lon],
-                popup=folium.Popup(f'<p>{r["id"]}</p><p>{r["updated"]}</p><p>{r["name"]}</p><p>{r["distance"]}</p>', max_width=300),
-                tooltip=f'<p>{r["id"]}</p><p>{r["updated"]}</p><p>{r["name"]}</p><p>{r["distance"]}</p>',
-                radius=800,
-                color="yellow",
-            )
-        )
+        for uploaded_file in uploaded_files
+    ]
 
-for i, r in df0.iterrows():
-    tag_map = f'<a href="https://www.google.com/maps?layer=c&cbll={r["緯度"]},{r["経度"]}" target="_blank" rel="noopener noreferrer">{r["場所"]}</a>'
-    fg3.add_child(
-        folium.Marker(
-            location=[r["緯度"], r["経度"]],
-            popup=folium.Popup(
-                f'<p>{tag_map}</p><p>{r["緯度"]}, {r["経度"]}</p><p>{r["eNB-LCID"]}</p><p>{r["更新日時"]}</p>',
-                max_width=300,
-            ),
-            icon=folium.Icon(color=r.color, icon=r.icon),
-            place=r["場所"],
-        )
+    # CSV結合
+    df0 = pd.concat(dfs, ignore_index=True)
+
+    # 日時変換
+    df0["measured_at"] = df0["measured_at"].dt.tz_convert("Asia/Tokyo").dt.tz_localize(None)
+    df0["discovered_at"] = df0["discovered_at"].dt.tz_convert("Asia/Tokyo").dt.tz_localize(None)
+
+    df0["cell_no"] = df0["short_cell_id"] & 0x3FFF
+
+    # タイプ別
+    df0["type"] = pd.cut(
+        df0["cell_no"],
+        bins=[0, 4000, 4500, 5000, 10000, 15000, 16384],
+        labels=["マクロセル", "ミニマクロ", "衛星エントランス", "Casa", "屋内局", "ピコセル"],
     )
 
-    radius = 780
-    
-    if r["設置タイプ"] == "屋内":
-        radius = 78
-    elif r["設置タイプ"] == "ピコセル":
-        radius = 312
+    ta_radius = st.number_input("TA", 0, 999, 150, step=1)
+    ta_satellite = st.number_input("衛星エントランスTA調整", 0, 999, 6, step=1)
 
-    if r["状況"] != "delete":
-        fg4.add_child(
-            folium.Circle(
+    # TA調整
+    df0["calibration"] = (
+        df0["type"]
+        .replace(
+            {"マクロセル": 0, "ミニマクロ": 0, "衛星エントランス": ta_satellite, "Casa": 0, "屋内局": 0, "ピコセル": 0}
+        )
+        .astype(int)
+    )
+
+    df0["short_cell_id"] = df0["short_cell_id"].astype(int)
+    df0["rnc"] = df0["rnc"].astype(int)
+
+    # バンド指定
+    df1 = df0.query("188743680 <= cell_id < 190023680").copy()
+
+    df1["id"] = df1["short_cell_id"].astype(str) + "-" + df1["rnc"].astype(str)
+
+    rsrp_max = df1.dropna(subset=["rsrp"]).groupby(["cell_id", "id", "psc"])["rsrp"].max()
+    ta_min = df1.dropna(subset=["ta"]).groupby(["cell_id", "id", "psc"])["ta"].min()
+
+    df2 = pd.concat([rsrp_max, ta_min], axis=1).sort_values("cell_id").reset_index()
+
+    df1["ta_adjusted"] = pd.to_numeric(df1["ta"], errors="coerce") - df1["calibration"]
+
+    df1["radius"] = (df1["ta_adjusted"] * ta_radius).fillna(0)
+
+    df1["ta"] = df1["ta"].fillna("-")
+
+    # 愛媛県の基地局
+
+    df_ehime = pd.read_csv(
+        "https://raku10ehime.github.io/map/ehime.csv",
+        index_col=0,
+        dtype={"sector": "Int64", "sub6": "Int64", "ミリ波": "Int64"},
+    )
+
+    df3 = df_ehime.dropna(subset=["eNB-LCID"])
+    df4 = enblcid_split(df3)
+
+    df5 = df2[~df2["cell_id"].isin(df4["cell_id"])].copy()
+    df6 = df1[~df1["cell_id"].isin(df4["cell_id"])].copy()
+
+    if len(df6) > 0:
+        st.dataframe(df5[["id", "psc", "rsrp", "ta"]], width=700, hide_index=True)
+
+        # ストリームリットセレクトボックスの作成
+        option = st.selectbox("どのeNB-LCIDを選択しますか？", df5["id"].unique())
+
+        df7 = df6[df6["id"] == option].copy()
+        df7["radius"] = df7["radius"].mask(df7["radius"] < 0, 0)
+
+        m = folium.Map(
+            location=[df7["lat"].mean(), df7["lon"].mean()],
+            tiles="https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png",
+            attr='&copy; <a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>',
+            zoom_start=14,
+        )
+
+        for _, r in df_ehime.iterrows():
+            folium.Marker(
                 location=[r["緯度"], r["経度"]],
-                radius=radius,
-                color="black",
-                weight = 1,
-                fill=True,
-                fill_opacity=0.5,
-            )
+                popup=folium.Popup(f'<p>{r["場所"]}</p>', max_width=300),
+                tooltip=r["場所"],
+                icon=folium.Icon(color=r["color"], icon=r["icon"]),
+            ).add_to(m)
+
+        for _, r in df7.iterrows():
+            if r["radius"] > 0:
+                folium.Circle(
+                    location=[r["lat"], r["lon"]],
+                    radius=r["radius"],
+                    color="red",
+                ).add_to(m)
+
+        # マップをストリームリットに表示
+        st_data = st_folium(m, width=700, height=500, returned_objects=[])
+
+        st.dataframe(
+            df7[["id", "psc", "rsrp", "ta", "cell_no", "ta_adjusted", "radius"]],
+            width=700,
+            hide_index=True,
         )
-
-for i, r in df2.iterrows():
-    fg5.add_child(
-        folium.Marker(
-            location=[r["緯度"], r["経度"]],
-            popup=folium.Popup(
-                f'<p>{r["場所"]}</p><p>{r["eNB-LCID"]}</p><p>{"<br />".join(r["更新日時"].split())}</p><p>{r["経過日数"]}</p>',
-                max_width=300,
-            ),
-            tooltip=f'<p>{r["eNB-LCID"]}</p><p>{"<br />".join(r["更新日時"].split())}</p><p>{r["経過日数"]}</p>',
-            icon=folium.plugins.BeautifyIcon(
-                icon_shape="circle-dot", border_width=5, border_color=r["past_days"]
-            ),
-        )
-    )
-
-# 検索
-folium.plugins.Search(
-    layer=fg3,
-    geom_type="Point",
-    placeholder="場所検索",
-    collapsed=True,
-    search_label="place",
-).add_to(map)
-
-folium.LayerControl().add_to(map)
-folium.plugins.LocateControl().add_to(map)
-
-# クリック位置の緯度・経度表示
-map.add_child(folium.LatLngPopup())
-
-# map
-
-map_path = pathlib.Path("map", "index.html")
-map_path.parent.mkdir(parents=True, exist_ok=True)
-map.save(map_path)
